66517fcb997c2aeb34b688b8365138dd070e0f35
GN-BOT-WORKFLOW <gn-bot-workflow@users.noreply.github.com>
2026-04-08 13:22:24 +0000

    automated commit

---
+++ js/config.js
@@ -0,0 +1,6 @@
+window.respecConfig = {
+  "generatedBy": "new-beheerportaal",
+  "generatedAt": "2026-04-08T13:22:12.362Z",
+  "repository": "word2markdown-handleiding",
+  "sourceFile": "Handleiding.docx"
+};
